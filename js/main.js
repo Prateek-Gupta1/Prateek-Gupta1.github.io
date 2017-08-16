f1d6314a9a6f4e7763bf01bb47a5e55ec69586c2
Prateek Gupta <guptaprateek328@gmail.com>
2017-08-15 23:37:22 -0700

    Website taking shape

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function responsiveNavMenuIcon(){
+	var x = document.getElementById("topnav");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
+
+function flipCard(){
+	var x = document.getElementById("flip-card");
+	if(x.className === ""){
+		x.className += "flipped";
+	}else{
+		x.className = "";
+	}
+}
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+var items = document.querySelectorAll(".timeline li");
+ 
+// code for the isElementInViewport function
+ 
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
