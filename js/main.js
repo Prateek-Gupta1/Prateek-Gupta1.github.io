b7e893ed26c7fb02ce51e26735fe6759e0aaf299
Prateek Gupta <guptaprateek328@gmail.com>
2017-08-17 17:37:23 -0700

    Added content in personal experience, moved skills at bottom, fixed footer, testing picture

--- js/main.js
+++ js/main.js
@@ -40,3 +40,16 @@ function callbackFunc() {
  
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 70
+        }, 1000);
+    }
+
+});
