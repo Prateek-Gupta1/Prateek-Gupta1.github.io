2d08ff07983f83de10dc5abca5d158767d4fd263
Prateek Gupta <guptaprateek328@gmail.com>
2017-08-18 13:29:00 -0700

    Website v1.0 completed. Made asthetic changes and added content in timeline.

--- js/main.js
+++ js/main.js
@@ -16,31 +16,6 @@ function flipCard(){
 	}
 }
 
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-var items = document.querySelectorAll(".timeline li");
- 
-// code for the isElementInViewport function
- 
-function callbackFunc() {
-  for (var i = 0; i < items.length; i++) {
-    if (isElementInViewport(items[i])) {
-      items[i].classList.add("in-view");
-    }
-  }
-}
- 
-window.addEventListener("load", callbackFunc);
-window.addEventListener("scroll", callbackFunc);
-
 $('a[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
@@ -48,7 +23,7 @@ $('a[href^="#"]').on('click', function(event) {
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: target.offset().top - 70
+            scrollTop: target.offset().top - 80
         }, 1000);
     }
 
